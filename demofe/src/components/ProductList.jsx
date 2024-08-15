2961c7fdb9fbc08b9338cddfd3c71827b552f088
Yunomi Xavia <yunomix2834@gmail.com>
2024-08-15 13:20:15 +0700

    Edit Last

--- demofe/src/components/ProductList.jsx
+++ demofe/src/components/ProductList.jsx
@@ -80,126 +80,138 @@ const ProductList = () => {
       <h1 className="mt-8 mb-10 text-5xl font-extrabold text-center text-gray-800">
         Product Management
       </h1>
-      <div className="flex justify-between mb-6">
-        <div className="flex items-center space-x-2">
-          <span className="font-semibold text-gray-700">Search products</span>
-          <input
-            type="text"
-            placeholder="Search products"
-            className="px-4 py-2 border border-blue-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-        </div>
-        <div className="flex items-center space-x-2">
-          <span className="font-semibold text-gray-700">Filter products</span>
-          <input
-            type="number"
-            placeholder="Filter by Category ID"
-            className="px-4 py-2 border border-blue-700 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
-            value={filterTerm}
-            onChange={handleFilter}
-          />
-        </div>
-        <button
-          className="flex items-center px-4 py-2 text-white transition-transform transform bg-green-300 rounded-lg hover:scale-105 active:scale-95"
-          onClick={handleNewProduct}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+      <div className="mx-4 mb-6 lg:mx-12">
+        <div className="flex justify-between mb-6">
+          <div className="flex space-x-4">
+            <div className="flex items-center space-x-2">
+              <span className="font-semibold text-gray-700">
+                Search products
+              </span>
+              <input
+                type="text"
+                placeholder="Search products"
+                className="px-4 py-2 border border-blue-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <span className="font-semibold text-gray-700">
+                Filter products
+              </span>
+              <input
+                type="number"
+                placeholder="Filter by Category ID"
+                className="px-4 py-2 border border-blue-700 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+                value={filterTerm}
+                onChange={handleFilter}
+              />
+            </div>
+          </div>
+          <button
+            className="flex items-center px-4 py-2 text-white transition-transform transform bg-green-300 rounded-lg hover:scale-105 active:scale-95"
+            onClick={handleNewProduct}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-          Create new product
-        </button>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border">
-          <thead>
-            <tr>
-              <th className="px-4 py-2 text-center text-gray-700 border">ID</th>
-              <th className="px-4 py-2 text-gray-700 border">Category ID</th>
-              <th className="px-4 py-2 text-gray-700 border">Name</th>
-              <th className="px-4 py-2 text-gray-700 border">Description</th>
-              <th className="px-4 py-2 text-center text-gray-700 border">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredProducts.length > 0 ? (
-              filteredProducts.map((product) => (
-                <tr key={product.id}>
-                  <td className="px-4 py-2 text-center border">{product.id}</td>
-                  <td className="px-4 py-2 text-center border">
-                    {product.category.id}
-                  </td>
-                  <td className="px-4 py-2 border">{product.name}</td>
-                  <td className="px-4 py-2 border">{product.description}</td>
-                  <td className="px-4 py-2 text-center border">
-                    <div className="flex justify-center space-x-2">
-                      <button
-                        className="flex items-center px-4 py-2 text-white transition-transform transform bg-blue-300 rounded-lg hover:scale-105 active:scale-95"
-                        onClick={() => handleEdit(product.id)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-6"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              className="w-5 h-5 mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+            Create new product
+          </button>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 text-center text-gray-700 border">
+                  ID
+                </th>
+                <th className="px-4 py-2 text-gray-700 border">Category ID</th>
+                <th className="px-4 py-2 text-gray-700 border">Name</th>
+                <th className="px-4 py-2 text-gray-700 border">Description</th>
+                <th className="px-4 py-2 text-center text-gray-700 border">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredProducts.length > 0 ? (
+                filteredProducts.map((product) => (
+                  <tr key={product.id}>
+                    <td className="px-4 py-2 text-center border">
+                      {product.id}
+                    </td>
+                    <td className="px-4 py-2 text-center border">
+                      {product.category.id}
+                    </td>
+                    <td className="px-4 py-2 border">{product.name}</td>
+                    <td className="px-4 py-2 border">{product.description}</td>
+                    <td className="px-4 py-2 text-center border">
+                      <div className="flex justify-center space-x-2">
+                        <button
+                          className="flex items-center px-4 py-2 text-white transition-transform transform bg-blue-300 rounded-lg hover:scale-105 active:scale-95"
+                          onClick={() => handleEdit(product.id)}
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                          />
-                        </svg>
-                        Edit
-                      </button>
-                      <button
-                        className="flex items-center px-4 py-2 text-white transition-transform transform bg-red-300 rounded-lg hover:scale-105 active:scale-95"
-                        onClick={() => handleDelete(product.id)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-6"
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            className="w-5 h-5 mr-2"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15.232 5.232l3.536 3.536M9 11l-4 4m0 0l4 4m-4-4h16"
+                            />
+                          </svg>
+                          Edit
+                        </button>
+                        <button
+                          className="flex items-center px-4 py-2 text-white transition-transform transform bg-red-300 rounded-lg hover:scale-105 active:scale-95"
+                          onClick={() => handleDelete(product.id)}
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                          />
-                        </svg>
-                        Delete
-                      </button>
-                    </div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            className="w-5 h-5 mr-2"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9 13h6m-6 4h6m2 4H7m4-4v-4m1-4h3m2-2.5L12 4 7 8.5m4-4v8m0 4v4m-1 4h2v2m-4-2h2"
+                            />
+                          </svg>
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="5" className="py-4 text-center">
+                    No products found
                   </td>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="5" className="py-4 text-center">
-                  No products found
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
